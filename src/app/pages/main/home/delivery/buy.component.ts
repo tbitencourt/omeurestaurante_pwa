67f2f04a7eba72d4dfd83fd13d781f5b3bd691c6
Rafael Felipe da Silva Pizão <rpizao@tecgraf.puc-rio.br>
2020-05-17 00:37:25 -0300

    Tela de compra da refeição funcionando como deveria. Próximo passo, integrar com whatsapp.

--- src/app/pages/main/home/delivery/buy.component.ts
+++ src/app/pages/main/home/delivery/buy.component.ts
@@ -1,10 +1,10 @@
-
 import { Component, OnInit } from '@angular/core';
-import { Product } from 'app/entities/product';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { DeliveryService } from 'app/services/delivery-service';
-import { ProductDetails } from 'app/entities/product-details';
 import { Delivery } from 'app/entities/delivery';
+import { Product } from 'app/entities/product';
+import { ProductDetails } from 'app/entities/product-details';
+import { DeliveryService } from 'app/services/delivery-service';
 @Component({
     selector: 'buy',
     templateUrl: './buy.component.html',
@@ -14,12 +14,41 @@ export class BuyComponent implements OnInit {
 
     details: ProductDetails;
 
+    productForm: FormGroup;
+
+    private optionalItemnsArray: FormArray;
+
     constructor(private actRoute: ActivatedRoute,
         private route: Router,
-        private deliveryService: DeliveryService) {
+        private deliveryService: DeliveryService,
+        private fb: FormBuilder) {
+
+        this.optionalItemnsArray = this.fb.array([]);
+
+        this.productForm = this.fb.group({
+            product: null,
+            notes: null,
+            requiredOption: [null, Validators.required],
+            optionalItemns: this.optionalItemnsArray
+        });
 
         const id = this.actRoute.snapshot.paramMap.get('id');
-        this.deliveryService.getProduct(Number.parseInt(id)).subscribe(p => this.details = p);
+        this.deliveryService.getProduct(Number.parseInt(id)).subscribe(details => {
+            let product = details.product;
+
+            if(product.yesOrNoOptions){
+                product.yesOrNoOptions.forEach(opt =>
+                    this.optionalItemnsArray.push(
+                        this.fb.group({
+                            [opt.name]: opt.selected
+                        })
+                    )
+                );
+            }
+            
+            this.details = details;
+            this.productForm.patchValue({product: product});
+        });
     }
 
     ngOnInit(): void {}
@@ -30,5 +59,13 @@ export class BuyComponent implements OnInit {
 
     get product(): Product {
         return this.details.product;
+    }
+
+    checkout(){
+        console.log(this.productForm.value);
+    }
+
+    formIsValid(): boolean {
+        return this.productForm.valid;
     }
 }
--- src/app/pages/main/home/delivery/buy.module.ts
+++ src/app/pages/main/home/delivery/buy.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
@@ -12,6 +12,7 @@ import { BuyComponent } from './buy.component';
     CommonModule,
     FormsModule,
     MatCardModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       { path: ':id', component: BuyComponent }
     ])
